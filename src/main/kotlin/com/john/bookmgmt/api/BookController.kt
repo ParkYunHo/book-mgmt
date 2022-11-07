7e4d158bc3958e348252d3fba067822cbef6fd29
ParkYunHo <qkrdbsgh0921@gmail.com>
2022-11-07 22:14:44 +0900

    [docs] springdoc-openapi 적용

--- src/main/kotlin/com/john/bookmgmt/api/BookController.kt
+++ src/main/kotlin/com/john/bookmgmt/api/BookController.kt
@@ -6,18 +6,21 @@ import com.john.bookmgmt.dto.BookDto
 import com.john.bookmgmt.dto.GenreDto
 import com.john.bookmgmt.exception.ParameterNotFoundException
 import com.john.bookmgmt.service.BookService
-import org.springframework.validation.annotation.Validated
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
 /**
  * @author yoonho
  * @since 2022.10.26
  */
+@Tag(name = "도서관리 시스템", description = "도서관리 시스템 API")
 @RestController
 class BookController(
     private val bookService: BookService
 ) {
 
+    @Operation(summary = "도서정보 조회")
     @GetMapping("/api/{type}")
     fun find(@PathVariable("type") type: String, @RequestParam id: String): BaseResponse {
         if(id.isNullOrBlank()){
@@ -33,6 +36,7 @@ class BookController(
         return BaseResponse().success(response)
     }
 
+    @Operation(summary = "도서정보 삭제")
     @DeleteMapping("/api/{type}")
     fun delete(@PathVariable("type") type: String, @RequestParam id: String): BaseResponse {
         if(id.isNullOrBlank()){
@@ -47,11 +51,13 @@ class BookController(
         return BaseResponse().successNoContent()
     }
 
+    @Operation(summary = "도서정보 저장")
     @PostMapping("/api/book")
     fun saveBook(@RequestBody input: BookDto): BaseResponse {
         return BaseResponse().success(bookService.saveBook(input, CommCode.Request.REST))
     }
 
+    @Operation(summary = "장르정보 저장")
     @PostMapping("/api/genre")
     fun saveGenre(@RequestBody input: GenreDto): BaseResponse {
         return BaseResponse().success(bookService.saveGenre(input, CommCode.Request.REST))
